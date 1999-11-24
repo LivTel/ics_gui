726898f9d25feea87e653e17fd8f95be4f252091
Chris Mottram <C.Mottram@ljmu.ac.uk>
1999-11-24 13:47:02 +0000

    Added author/version tags.

--- java/IcsGUIDialogListener.java
+++ java/IcsGUIDialogListener.java
@@ -1,5 +1,5 @@
 // CcsGUIDialogListener.java -*- mode: Fundamental;-*-
-// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIDialogListener.java,v 0.1 1999-11-22 09:53:49 cjm Exp $
+// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIDialogListener.java,v 0.2 1999-11-24 13:47:02 cjm Exp $
 import java.lang.*;
 import java.io.*;
 import java.util.*;
@@ -10,13 +10,15 @@ import ngat.message.base.*;
 
 /**
  * This class is an GUIDialogListener for the CcsGUI Dialog boxs.
+ * @author Chris Mottram
+ * @version $Revision: 0.2 $
  */
 public class CcsGUIDialogListener implements GUIDialogListener
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: IcsGUIDialogListener.java,v 0.1 1999-11-22 09:53:49 cjm Exp $");
+	public final static String RCSID = new String("$Id: IcsGUIDialogListener.java,v 0.2 1999-11-24 13:47:02 cjm Exp $");
 	/**
 	 * The main class reference.
 	 */
@@ -44,4 +46,7 @@ public class CcsGUIDialogListener implements GUIDialogListener
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 0.1  1999/11/22 09:53:49  cjm
+// initial revision.
+//
 //
