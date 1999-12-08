29a6a54d4f0d67ccdfcff775178317bf46439ae2
Chris Mottram <C.Mottram@ljmu.ac.uk>
1999-12-08 10:48:29 +0000

    initial revision.

---
+++ java/IcsGUIConfigButtonListener.java
@@ -0,0 +1,68 @@
+// CcsGUIConfigButtonListener.java -*- mode: Fundamental;-*-
+// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIConfigButtonListener.java,v 0.1 1999-12-08 10:48:29 cjm Exp $
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+/**
+ * This class is an ActionListener for the CcsGUI CONFIGDialog config button. It should bring up a 
+ * CcsCCDConfigListDialog, and any selected configs should go back to the CONFIGDialog.
+ * @author Chris Mottram
+ * @version $Revision: 0.1 $
+ */
+public class CcsGUIConfigButtonListener implements ActionListener
+{
+	/**
+	 * Revision Control System id string, showing the version of the Class.
+	 */
+	public final static String RCSID = new String("$Id: IcsGUIConfigButtonListener.java,v 0.1 1999-12-08 10:48:29 cjm Exp $");
+	/**
+	 * The instance of the main program.
+	 */
+	private CcsGUI parent = null;
+	/**
+	 * The CONFIGDialog that wants to bring up the CcsCCDConfigListDialog.
+	 */
+	private CONFIGDialog configDialog = null;
+	/**
+	 * The list dialog to be brought up when the button is pressed.
+	 */
+	private CcsCCDConfigListDialog configListDialog = null;
+
+	/**
+	 * Constructor. This sets the parent to be main program class.
+	 * It constructs the CcsCCDConfigListDialog to be made visible when the button is pressed.
+	 * @param p The parent object which owns the menu items.
+	 * @see #parent
+	 * @see #configListDialog
+	 */
+	public CcsGUIConfigButtonListener(CcsGUI p,CONFIGDialog cd)
+	{
+		super();
+
+		parent = p;
+		configDialog = cd;
+		configListDialog = new CcsCCDConfigListDialog(p.getFrame(),p.getStatus().getCCDConfigProperties());
+	}
+
+
+	/**
+	 * Routine that is called when the config button is pressed.
+	 * @param event The event that caused this call to the listener.
+	 */
+	public void actionPerformed(ActionEvent event)
+	{
+		configListDialog.setLocation(configDialog.getX()+configDialog.getWidth(),configDialog.getY());
+		configListDialog.setConfigDialog(configDialog);
+		configListDialog.setData();
+		configListDialog.pack();
+		configListDialog.setVisible(true);
+	}
+}
+//
+// $Log: not supported by cvs2svn $
+//
