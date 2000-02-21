dfc7677892f3180b413d28f09b6797534c8f40e7
Chris Mottram <C.Mottram@ljmu.ac.uk>
2000-02-21 10:46:48 +0000

    Added more prints for EXPOSE/CALIBRATE_DONE and MULTRUN_DP_ACK.

--- java/IcsGUIClientConnectionThread.java
+++ java/IcsGUIClientConnectionThread.java
@@ -1,5 +1,5 @@
 // CcsGUIClientConnectionThread.java -*- mode: Fundamental;-*-
-// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIClientConnectionThread.java,v 0.2 1999-12-09 17:02:12 cjm Exp $
+// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIClientConnectionThread.java,v 0.3 2000-02-21 10:46:48 cjm Exp $
 
 import java.lang.*;
 import java.io.*;
@@ -10,6 +10,9 @@ import ngat.net.*;
 import ngat.message.base.*;
 import ngat.message.ISS_INST.MOVIE_ACK;
 import ngat.message.ISS_INST.MULTRUN_ACK;
+import ngat.message.ISS_INST.MULTRUN_DP_ACK;
+import ngat.message.ISS_INST.CALIBRATE_DONE;
+import ngat.message.ISS_INST.EXPOSE_DONE;
 import ngat.message.ISS_INST.GET_STATUS_DONE;
 
 /**
@@ -17,14 +20,14 @@ import ngat.message.ISS_INST.GET_STATUS_DONE;
  * It implements the generic ISS instrument command protocol.
  * It is used to send commands from the CcsGUI to the Ccs.
  * @author Chris Mottram
- * @version $Revision: 0.2 $
+ * @version $Revision: 0.3 $
  */
 public class CcsGUIClientConnectionThread extends TCPClientConnectionThreadMA
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: IcsGUIClientConnectionThread.java,v 0.2 1999-12-09 17:02:12 cjm Exp $");
+	public final static String RCSID = new String("$Id: IcsGUIClientConnectionThread.java,v 0.3 2000-02-21 10:46:48 cjm Exp $");
 	/**
 	 * The CcsGUI object.
 	 */
@@ -64,6 +67,8 @@ public class CcsGUIClientConnectionThread extends TCPClientConnectionThreadMA
 			printMovieAck((MOVIE_ACK)acknowledge);
 		if(acknowledge instanceof MULTRUN_ACK)
 			printMultrunAck((MULTRUN_ACK)acknowledge);
+		if(acknowledge instanceof MULTRUN_DP_ACK)
+			printMultrunDpAck((MULTRUN_DP_ACK)acknowledge);
 	}
 
 	/**
@@ -87,8 +92,21 @@ public class CcsGUIClientConnectionThread extends TCPClientConnectionThreadMA
 	}
 
 	/**
+	 * Routine to print out more details if the acknowledge was a MULTRUN_DP_ACK. Note the returned filename
+	 * will have already been printed out by printMultrunAck, as MULTRUN_DP_ACK is a sub-class of MULTRUN_ACK.
+	 * @param multrunDpAck The acknowledge object passed back to the client.
+	 */
+	private void printMultrunDpAck(MULTRUN_DP_ACK multrunDpAck)
+	{
+		parent.log("Seeing:"+multrunDpAck.getSeeing()+":Brightest Counts:"+multrunDpAck.getCounts()+
+			":Brightest X Pixel:"+multrunDpAck.getXpix()+":Brightest Y Pixel:"+multrunDpAck.getYpix());
+	}
+
+	/**
 	 * This routine processes the done object returned by the server. Currently prints out
-	 * the basic return values in done.
+	 * the basic return values in done. If done is an instance of GET_STATUS_DONE calls printGetStatus
+	 * to print out more informatiom.
+	 * @see #printGetStatus
 	 */
 	protected void processDone()
 	{
@@ -100,11 +118,18 @@ public class CcsGUIClientConnectionThread extends TCPClientConnectionThreadMA
 		else
 		{
 			parent.log("Done message received for:"+command.getClass().getName()+":"+
-				"\nerror Number:"+done.getErrorNum()+
-				"\nerror String:"+done.getErrorString()+
-				"\nsuccessful:"+done.getSuccessful());
+				"successful:"+done.getSuccessful());
+			if(done.getSuccessful() == false)
+			{
+				parent.log("Error:error Number:"+done.getErrorNum()+
+					"\nerror String:"+done.getErrorString());
+			}
 			if(done instanceof GET_STATUS_DONE)
-				printGetStatus((GET_STATUS_DONE)done);
+				printGetStatusDone((GET_STATUS_DONE)done);
+			if(done instanceof CALIBRATE_DONE)
+				printCalibrateDone((CALIBRATE_DONE)done);
+			if(done instanceof EXPOSE_DONE)
+				printExposeDone((EXPOSE_DONE)done);
 		}
 		parent.getStatus().removeClientThread(this);
 	}
@@ -121,28 +146,79 @@ public class CcsGUIClientConnectionThread extends TCPClientConnectionThreadMA
 		parent.error(errorString);
 	}
 
-	private void printGetStatus(GET_STATUS_DONE getStatusDone)
+	/**
+	 * Method to display some status for a GET_STATUS message.
+	 * @param getStatusDone The DONE object instance containing status data to display.
+	 * @see #processDone
+	 */
+	private void printGetStatusDone(GET_STATUS_DONE getStatusDone)
 	{
 		Hashtable displayInfo = null;
+		Integer integer = null;
+		int exposureCount,exposureNumber;
 
+	// current mode
 		parent.log("The current mode:"+getStatusDone.getCurrentMode()+".");
-		parent.setCCDStatus(""+getStatusDone.getCurrentMode());
+		parent.setCCDStatusLabel(getStatusDone.getCurrentMode());
+	// displayInfo hash table
 		displayInfo = getStatusDone.getDisplayInfo();
 		if(displayInfo == null)
 		{
 			parent.log("The display information was null.");
 			return;
 		}
+	// log contents of hash table
 		for (Enumeration e = displayInfo.keys(); e.hasMoreElements();)
 		{
 			Object key = e.nextElement();
 			Object value = displayInfo.get(key);
 			parent.log(key.toString()+":"+value.toString());
 		}
+	// set current command status
+		parent.setCurrentCommandLabel((String)(displayInfo.get("currentCommand")));
+	// set filters selected status
+		parent.setFiltersSelectedLabel((String)(displayInfo.get("Filter Wheel:0")),
+			(String)(displayInfo.get("Filter Wheel:1")));
+	// set remaining exposures status
+		integer = (Integer)(displayInfo.get("Exposure Count"));
+		exposureCount = integer.intValue();
+		integer = (Integer)(displayInfo.get("Exposure Number"));
+		exposureNumber = integer.intValue();
+		if(exposureCount <= 0)// special case MOVIE - or not exposing
+			parent.setRemainingExposuresLabel(0);
+		else
+			parent.setRemainingExposuresLabel(exposureCount-exposureNumber);
+	}
+
+	/**
+	 * Method to print out parameters associated with a CALIBRATE_DONE (sub)class instance.
+	 * These parameters are <i>peakCounts</i> and <i>meanCounts</i>.
+	 * @param calibrateDone The instance of the DONE message.
+	 */
+	private void printCalibrateDone(CALIBRATE_DONE calibrateDone)
+	{
+		parent.log("peakCounts:"+calibrateDone.getPeakCounts()+
+			":meanCounts:"+calibrateDone.getMeanCounts());
+	}
+
+	/**
+	 * Method to print out parameters associated with a EXPOSE_DONE (sub)class instance.
+	 * These parameters are <i>Seeing</i>, <i>Counts</i>, <i>X_pix</i> and <i>Y_pix</i>.
+	 * @param exposeDone The instance of the DONE message.
+	 */
+	private void printExposeDone(EXPOSE_DONE exposeDone)
+	{
+		parent.log("Seeing:"+exposeDone.getSeeing()+
+			":Brightest Counts:"+exposeDone.getCounts()+
+			":Brightest X Pixel:"+exposeDone.getXpix()+
+			":Brightest Y Pixel:"+exposeDone.getYpix());
 	}
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 0.2  1999/12/09 17:02:12  cjm
+// More functionality added.
+//
 // Revision 0.1  1999/11/22 09:53:49  cjm
 // initial revision.
 //
