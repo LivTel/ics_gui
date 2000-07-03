2a51fcc2ebc67180578b105ea7a4001f10d13e6f
Chris Mottram <C.Mottram@ljmu.ac.uk>
2000-07-03 10:33:09 +0000

    Level of GET_STATUS now determined by ccs_gui.auto_update.get_status.level property.

--- java/IcsGUIUpdateThread.java
+++ java/IcsGUIUpdateThread.java
@@ -1,5 +1,5 @@
 // CcsGUIUpdateThread.java -*- mode: Fundamental;-*-
-// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIUpdateThread.java,v 0.4 2000-05-22 11:14:06 cjm Exp $
+// $Header: /home/cjm/cvs/ics_gui/java/IcsGUIUpdateThread.java,v 0.5 2000-07-03 10:33:09 cjm Exp $
 
 import java.lang.*;
 /**
@@ -8,14 +8,14 @@ import java.lang.*;
  * It then calls parent.sendCommand(GET_STATUS) to update the GUI's status.
  * This continues until the thread is quit.
  * @author Chris Mottram
- * @version $Revision: 0.4 $
+ * @version $Revision: 0.5 $
  */
 public class CcsGUIUpdateThread extends Thread
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: IcsGUIUpdateThread.java,v 0.4 2000-05-22 11:14:06 cjm Exp $");
+	public final static String RCSID = new String("$Id: IcsGUIUpdateThread.java,v 0.5 2000-07-03 10:33:09 cjm Exp $");
 	/**
 	 * The CcsGUI object.
 	 */
@@ -57,16 +57,26 @@ public class CcsGUIUpdateThread extends Thread
 		ngat.message.ISS_INST.GET_STATUS command = null;
 		CcsGUIClientConnectionThread thread = null;
 		String idString = null;
-		int id;
+		int id,level;
 
 		id = 0;
+		try
+		{
+			level = parent.getStatus().getPropertyInteger("ccs_gui.auto_update.get_status.level");
+		}
+		catch(NumberFormatException e)
+		{
+			parent.error("Auto-update:Invalid GET_STATUS level:Using zero.");
+			level = 0;
+		}
 		while(!quit)
 		{
 		// send the command
 			idString = new String(this.getClass().getName()+":"+id);
 			command = new ngat.message.ISS_INST.GET_STATUS(idString);
-			command.setLevel(0);
-			parent.log("Auto-update sending command:"+command.getClass().getName());
+			command.setLevel(level);
+			parent.log("Auto-update sending command:"+command.getClass().getName()+
+				":level="+command.getLevel());
 			thread = parent.sendCommand(command);
 			while(thread.isAlive())
 			{
@@ -102,6 +112,10 @@ public class CcsGUIUpdateThread extends Thread
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 0.4  2000/05/22 11:14:06  cjm
+// Swapped round position of sleep to make stopping
+// /starting the thread look more responsive.
+//
 // Revision 0.3  2000/03/10 11:57:10  cjm
 // The run method now waits for the currently spawned GET_STATUS client thread to terminate before
 // entering the sleep for the next one. This stops the GUI spawning lots of client threads
